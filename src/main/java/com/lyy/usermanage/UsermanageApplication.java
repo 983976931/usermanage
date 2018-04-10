a026f3d4aa1c82e86434de3d87f636e6dd5b51b8
leeyangyang <983976931@qq.com>
2018-04-10 20:26:14 +0800

    第一次提交

---
+++ src/main/java/com/lyy/usermanage/UsermanageApplication.java
@@ -0,0 +1,12 @@
+package com.lyy.usermanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanageApplication.class, args);
+	}
+}
